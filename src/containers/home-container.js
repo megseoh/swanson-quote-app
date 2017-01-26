372b02d872e2919ae6233e14bd36078ca2a0d5ff
megseoh <ohara.meagan@gmail.com>
2017-01-26 12:12:13 -0500

    changed form to link on home page

--- src/containers/home-container.js
+++ src/containers/home-container.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import Header from '../components/header.js';
 import '../App.css';
 
@@ -7,6 +8,7 @@ export default class HomeContainer extends Component {
 		return (
 			<div className="App">
 				<Header />
+				<Link className="link-quote" to='/quote'>What would Ron say?</Link>
 			</div>
 		);
 	}
