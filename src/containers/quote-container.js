f92703c64c10f1d4b73c96cf9005d8aad072ad83
megseoh <ohara.meagan@gmail.com>
2017-01-26 14:05:39 -0500

    added colour swatches at bottom of quote screen

--- src/containers/quote-container.js
+++ src/containers/quote-container.js
@@ -45,12 +45,21 @@ export default class QuoteContainer extends Component {
 							<img src={ ron } className="ron-image" alt="Ron Swanson" />
 							<Quote quote={ this.state.quote }/>
 						</div>
+						<form onSubmit={ this.handleSubmit }
+						>
+							<input type="submit" value="Click for a new quote" />
+						</form>
+						<div className="colors-container">
+							<div className="select-color" style={{ background: '#DA4C45' }}></div>
+							<div className="select-color" style={{ background: '#7796A0' }}></div>
+							<div className="select-color" style={{ background: '#FC7C8B' }}></div>
+							<div className="select-color" style={{ background: '#82B5A0' }}></div>
+							<div className="select-color" style={{ background: '#BB99C2' }}></div>
+							<div className="select-color" style={{ background: '#D3D88A' }}></div>
+							<div className="select-color" style={{ background: '#5f9ea0' }}></div>
+						</div>
 					</div>
 				</div>
-				<form onSubmit={ this.handleSubmit }
-				>
-					<input type="submit" value="Click for a new quote" />
-				</form>
 			</div>
 		);
 	}
